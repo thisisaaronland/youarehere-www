77c523b9253e7e6fe244f3c4ff872d9fd1988ab5
thisisaaronland <aaron@mapzen.com>
2015-06-18 00:51:38 +0000

    stub out nearby method

--- www/include/lib_api_mapzen_youarehere.php
+++ www/include/lib_api_mapzen_youarehere.php
@@ -1,6 +1,7 @@
 <?php
 
 	loadlib("mapzen_pelias");
+	loadlib("geo_utils");
 
 	########################################################################
 
@@ -24,6 +25,37 @@
 
 		$data = $rsp['data'];
 		api_output_ok($data);
+	}
+
+	########################################################################
+
+	function api_mapzen_youarehere_nearby(){
+
+		$lat = request_float("latitude");
+		$lon = request_float("longitude");
+
+		$lat = (geo_utils_is_valid_latitude($lat)) ? $lat : null;
+		$lon = (geo_utils_is_valid_longitude($lon)) ? $lon : null;
+
+		if (! $lat){
+			api_output_error(400, "Missing or invalid latitude");
+		}
+
+		if (! $lon){
+			api_output_error(400, "Missing or invalid longitude");
+		}
+
+		# filter / zoom level
+
+		$more = array('layers' => 'admin');
+		$rsp = mapzen_pelias_reverse_geocode($lat, $lon);
+
+		if (! $rsp['ok']){
+			api_output_error(500, $rsp['error']);
+		}
+
+		$data = $rsp['data'];
+		api_output_ok($data);		
 	}
 
 	########################################################################
